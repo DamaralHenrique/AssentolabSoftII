49d5e77e8a3378fe2a9207c96f03150e70a952f7
DamaralHenrique <rique01101999@gmail.com>
2024-04-17 17:54:05 -0300

    Adiciona rotina de get de assentos

--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'assentos.html', {})
+    estadio_id = 1 #request.GET['estadio_id']
+    lugares = list(get_lugares(estadio_id))
+    # print(lugares)
+    return render(request, 'assentos.html', {'lugares':lugares})
+
+def get_lugares(estadio_id):
+
+    print("get_lugares")
+
+    payload = {'estadio_id': estadio_id}
+    resp = requests.get('http://172.17.0.2:8000/lugares', params=payload)
+
+    # print(resp.status)
+    resp = resp.json()
+
+    print(resp)
+    
+    return resp
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t1xk@ehxlml#3i^bhvl#^!1+76l5@luga!ys7t=6+cfe*et7(i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.17.0.3', '127.0.0.1', '172.17.0.2', '127.0.0.1']
 
 
 # Application definition
